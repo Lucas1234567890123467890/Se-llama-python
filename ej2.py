ee85be59151d127512bfbc3e7f30a92afd5618d5
Lucas1234567890123467890 <lucaslongo99@gmail.com>
2025-05-07 15:10:21 -0300

    Update ej2.py

--- ej2.py
+++ ej2.py
@@ -1,10 +1,11 @@
-print("ingrese el primer numero");
-num1=int(input());
-print("ingrese el segundo numero");
-num2=int(input());
-print("ingrese el tercer numero");
-num3=int(input());
-print("ingrese el cuarto numero");
-num4=int(input());
-R=num1+num2+num3+num4;
-print("El resultado es:", R)
+def eje2():
+  print("ingrese el primer numero")
+  num1=int(input())
+  print("ingrese el segundo numero")
+  num2=int(input())
+  print("ingrese el tercer numero")
+  num3=int(input())
+  print("ingrese el cuarto numero")
+  num4=int(input())
+  R=num1+num2+num3+num4
+  print("El resultado es:", R)
