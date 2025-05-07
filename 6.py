f38021b998239ffa80fd02775c20991fdac1b860
Lucas1234567890123467890 <lucaslongo99@gmail.com>
2025-05-07 15:16:37 -0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,5 +1,6 @@
-base = float(input("Ingrese la base del triángulo: "))
-altura = float(input("Ingrese la altura del triángulo: "))
-medio = 0.5
-superficie = medio * base * altura
-print("La superficie del triángulo es:", superficie)
+def 6():
+  base = float(input("Ingrese la base del triángulo: "))
+  altura = float(input("Ingrese la altura del triángulo: "))
+  medio = 0.5
+  superficie = medio * base * altura
+  print("La superficie del triángulo es:", superficie)
