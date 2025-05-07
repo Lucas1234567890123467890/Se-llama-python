f0bf6713914e4b9c336c29c7c2597c87ea10df38
Lucas1234567890123467890 <lucaslongo99@gmail.com>
2025-05-07 15:17:29 -0300

    8.py

--- 8.py
+++ 8.py
@@ -1,4 +1,5 @@
-Cant_parcial = int(input("Ingrese la cantidad parcial: "))
-Cant_total = int(input("Ingrese la cantidad total: "))
-porcentaje = (Cant_parcial * 100) / Cant_total
-print("El porcentaje es:", porcentaje)
+def 8():
+  Cant_parcial = int(input("Ingrese la cantidad parcial: "))
+  Cant_total = int(input("Ingrese la cantidad total: "))
+  porcentaje = (Cant_parcial * 100) / Cant_total
+  print("El porcentaje es:", porcentaje)
