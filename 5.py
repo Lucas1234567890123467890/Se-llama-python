3c1517b4e4ec56d3526653d5aee01c71d3ae01f5
Lucas1234567890123467890 <lucaslongo99@gmail.com>
2025-05-07 15:15:50 -0300

    5.py

--- 5.py
+++ 5.py
@@ -1,5 +1,6 @@
-horas = int(input("Ingrese las horas: "))
-minutos = int(input("Ingrese los minutos: "))
-segundos = int(input("Ingrese los segundos: "))
-Seg_Max = horas * 3600 + minutos * 60 + segundos
-print("El total en segundos es:", Seg_Max)
+def 5():
+  horas = int(input("Ingrese las horas: "))
+  minutos = int(input("Ingrese los minutos: "))
+  segundos = int(input("Ingrese los segundos: "))
+  Seg_Max = horas * 3600 + minutos * 60 + segundos
+  print("El total en segundos es:", Seg_Max)
