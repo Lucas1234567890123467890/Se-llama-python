cc85efe3293a4da851dd5691aae9d0e629d5e929
Lucas1234567890123467890 <lucaslongo99@gmail.com>
2025-05-07 15:18:42 -0300

    9.py

--- 9.py
+++ 9.py
@@ -1,7 +1,8 @@
-fecha = int(input("Ingrese una fecha: "))
-anio = fecha % 10000
-mes = (fecha // 10000) % 100
-dia = fecha // 1000000
-print("Día:", dia)
-print("Mes:", mes)
-print("Año:", año)
+def 9():
+  fecha = int(input("Ingrese una fecha: "))
+  año = fecha % 10000
+  mes = (fecha // 10000) % 100
+  dia = fecha // 1000000
+  print("Día:", dia)
+  print("Mes:", mes)
+  print("Año:", año)
